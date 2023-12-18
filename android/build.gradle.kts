0fcf544ea4c9e9cccf5e499053cd0ba7e1059da2
fengjianhao <lingj.jhf@outlook.com>
2023-12-18 10:44:21 +0800

    groovy转换kts

---
+++ android/build.gradle.kts
@@ -0,0 +1,32 @@
+buildscript {
+    extra.set("kotlin_version","1.7.10")
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${rootProject.extra["kotlin_version"]}")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+val output: Provider<Directory> = rootProject.layout.buildDirectory.dir("../../build")
+
+subprojects {
+    project.layout.buildDirectory.dir("${output.get().asFile}/${project.name}")
+}
+
+subprojects {
+    project.evaluationDependsOn(":app")
+}
+
+tasks.register<Delete>("clean") {
+    delete(output)
+}
